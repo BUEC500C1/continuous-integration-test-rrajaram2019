e92a0f7e8a15d31bff232fa562bada6fc49be0de
rrajaram2019 <51894481+rrajaram2019@users.noreply.github.com>
2021-01-27 11:34:09 -0500

    Update UnitConverter.py

--- UnitConverter.py
+++ UnitConverter.py
@@ -1,7 +1,28 @@
-def lint_test(inp):
-  out=inp*2
-  
+import numpy as np
+# ============================================================
+# Defining your own functions here
+# ============================================================
 
-def test_func():
-  assert lint_test(3)==6
-  assert lint_test(5)==10
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def numpy_add(a, b):
+    return np.add(a, b)
+
+def numpy_subtract(a, b):
+    return np.subtract(a, b)
+
+# ============================================================
+# Defining your own testing here
+# ============================================================
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add('boston', 'university') == 'bostonuniversity'
+
+
+def test_subtract():
+    assert subtract(2, 3) == -1
