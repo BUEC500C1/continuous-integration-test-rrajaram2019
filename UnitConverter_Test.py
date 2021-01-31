d976b7601336bc871958607d8a8e5062641e9d6d
rrajaram2019 <51894481+rrajaram2019@users.noreply.github.com>
2021-01-31 17:10:07 -0500

    Update UnitConverter_Test.py

--- UnitConverter_Test.py
+++ UnitConverter_Test.py
@@ -2,7 +2,7 @@ from UnitConverter import *
 import pytest
 
 def test_distance():
-  assert myParser("1 foot to inches") == "12.0 inches"
+  assert myParser("1 foot into inches") == "12.0 inches"
 
 def test_weight():
   assert myParser("convert 1 kg into ounces") == "12.0 inches"
