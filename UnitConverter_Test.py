46b1b459b10a234352ab08459469a42e99a938d7
rrajaram2019 <51894481+rrajaram2019@users.noreply.github.com>
2021-01-31 17:12:07 -0500

    Update UnitConverter_Test.py

--- UnitConverter_Test.py
+++ UnitConverter_Test.py
@@ -5,7 +5,7 @@ def test_distance():
   assert myParser("1 foot to inches") == "12.0 inches"
 
 def test_weight():
-  assert myParser("convert 1 kg into ounces") == "12.0 inches"
+  assert myParser("convert 1 kg into ounces") == "35.3 ounces"
 
 def test_temperature():
   assert myParser("what is 1 celcius in fahrenheit?") == "33.8 fahrenheit"
