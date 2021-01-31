44f42fddab5734413538da3323bce2c09cf8144c
rrajaram2019 <51894481+rrajaram2019@users.noreply.github.com>
2021-01-31 15:54:29 -0500

    Update UnitConverter.py

--- UnitConverter.py
+++ UnitConverter.py
@@ -10,15 +10,15 @@ def dist_conv(amt,inp_unit,out_unit):
     miles=0.000621371
     if inp_unit=="feet":
         amt=amt/feet
-    if inp_unit=="inch":
+    if inp_unit=="inches":
         amt=amt/inches
-    if inp_unit=="mile":
+    if inp_unit=="miles":
         amt=amt/miles
     if out_unit=="feet":
         amt=amt*feet
-    if out_unit=="inch":
+    if out_unit=="inches":
         amt=amt*inches
-    if out_unit=="mile":
+    if out_unit=="miles":
         amt=amt*miles
     return amt
 
@@ -27,20 +27,20 @@ def weight_conv(amt,inp_unit,out_unit):
     kilogram=1.0
     ounces=35.274
     pounds=2.20462
-    if inp_unit=="ounce":
+    if inp_unit=="ounces":
         amt=amt/ounces
-    if inp_unit=="pound":
+    if inp_unit=="pounds":
         amt=amt/pounds
-    if out_unit=="ounce":
+    if out_unit=="ounces":
 
         amt=amt*ounces
-    if out_unit=="pound":
+    if out_unit=="pounds":
         amt=amt*pounds
     return amt
 
 def temp_conv(amt,inp_unit,out_unit):
-    celcius=(9/5)*amt+32
-    fahrenheit=(amt-32)/1.8
+    fahrenheit=(9/5)*amt+32
+    celcius=(amt-32)/1.8
     if out_unit=="fahrenheit":
         return fahrenheit
     if out_unit=="celcius":
@@ -49,67 +49,64 @@ def temp_conv(amt,inp_unit,out_unit):
 def find_units(request):
     unit1=""
     unit2=""
+    unit_type=""
     flag=0
-    sec_u=0
+    signal=0
+    hold=9223372036854775807
+    hold2=0
     distance=[["eter"," m "],["eet","ft","oot"],["nch","in"],["mi","ile"]]
-    dist=["meter","feet","inch","mile"]
+    dist=["meters","feet","inches","miles"]
     weight=[["ilogram","Kg","kg"],["unce","oz"],["lb","pound"]]
-    wght=["kilogram","ounce","pound"]
+    wght=["kilograms","ounces","pounds"]
     temperature=[["elcius"," C "],["ahrenheit"," F "]]
     temp=["celcius","fahrenheit"]
     unit_type=""
-    for b in range(len(distance)-1):
+    for b in range(len(distance)):
         u=distance[b]
         unit_type="distance"
         for x in u:
-
-            if flag==1:
-                sec_u=1
-            if request.find(x)>-1 & flag==0:
-
+            if -1<request.find(x)<hold:
+                hold=request.find(x)
                 unit1=dist[b]
-                flag=1
-            if request.find(x)>-1 & sec_u==1:
 
+        for y in u:
+            if request.find(y)>hold2:
+                hold2=request.find(y)
+                signal=1
                 unit2=dist[b]
-                sec_u=2
-                flag=2
 
-    if flag!=2:
-        for b in range(len(weight)-1):
+
+    if signal==0:
+        for b in range(len(weight)):
             u=weight[b]
             unit_type="weight"
             for x in u:
+                for x in u:
+                    if -1<request.find(x)<hold:
+                        hold=request.find(x)
+                        unit1=wght[b]
 
-                if flag==1:
-                    sec_u=1
-                if request.find(x)>-1 & flag==0:
+                for y in u:
+                    if request.find(y)>hold2:
+                        hold2=request.find(y)
+                        signal=1
+                        unit2=wght[b]
 
-                    unit1=wght[b]
-                    flag=1
-                if request.find(x)>-1 & sec_u==1:
-
-                    unit2=wght[b]
-                    sec_u=2
-                    flag=2
-
-    if flag!=2:
-        for b in range(len(temperature)-1):
+    if signal==0:
+        for b in range(len(temperature)):
             u=temperature[b]
             unit_type="temperature"
             for x in u:
+                for x in u:
+                    if -1<request.find(x)<hold:
+                        hold=request.find(x)
+                        unit1=temp[b]
 
-                if flag==1:
-                    sec_u=1
-                if request.find(x)>-1 & flag==0:
-
-                    unit1=temp[b]
-                    flag=1
-                if request.find(x)>-1 & sec_u==1:
-
-                    unit2=temp[b]
-                    sec_u=2
-                    flag=2
+                for y in u:
+                    if request.find(y)>hold2:
+                        hold2=request.find(y)
+                        signal=1
+                        unit2=temp[b]
 
     return [unit1,unit2,unit_type]
 
@@ -119,19 +116,17 @@ def myParser(txt):
     input_u=units[0]
     output_u=units[1]
     un_type=units[2]
+
     myflag=0
     num1=0
     num2=0
-    for i in range(len(txt)-1):
+    for i in range(len(txt)):
         if txt[i].isdigit() and myflag==0:
-
             num1=i
             myflag=1
-        else:
-            if txt[i].isdigit() and myflag==1:
-
+        if txt[i].isdigit() and myflag==1:
                 num2=i
-    final_num=float(txt[num1:num2])
+    final_num=float(txt[num1:num2+1])
     des_out=0
     if un_type == "distance":
         des_out=dist_conv(final_num,input_u,output_u)
@@ -139,11 +134,14 @@ def myParser(txt):
         des_out=weight_conv(final_num,input_u,output_u)
     if un_type == "temperature":
         des_out=temp_conv(final_num,input_u,output_u)
-    print(des_out,output_u)
+    if (input_u is output_u):
+        print("ERROR: You have entered incorrect units")
+    else:
+        print(des_out,output_u)
 
 # ============================================================
 # Defining your own testing here
 # ============================================================
 
-user_txt="12 inches to mile"
+user_txt="1 celcius to fahrenheit"
 myParser(user_txt)
